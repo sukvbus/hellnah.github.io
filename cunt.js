919c42e9d369c7cfaa5007385424a2b9fedabd11
sukvbus <gameovledd@gmail.com>
2025-09-03 13:25:32 +0700

    Add files via upload

---
+++ cunt.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+  document.querySelector(".atts").classList.add("show");
+});
+
+window.addEventListener("load", function() {
+  document.querySelector(".helllogo").classList.add("show");
+});
